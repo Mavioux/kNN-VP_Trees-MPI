78bd4efa3dab3eef7ea1be2af35c0a4564e9d87e
mavioux <pakmarios@gmail.com>
2020-12-25 07:44:25 +0200

    fixed the sub_knn bug, position appropriately the elements in knnResult

--- src/v1.c
+++ src/v1.c
@@ -90,7 +90,7 @@ knnresult kNN(double * X, double * Y, int n, int m, int d, int k, int rank){
             sum += d_matrix[j + m*i];
         }
     }
-    printf("%d %f\n",rank, sum);
+    // printf("%d %f\n",rank, sum);
 
     // We have in our hands the D matrix in row major format
     // Next we have to search each column for the kNN    
@@ -139,7 +139,8 @@ void main() {
 
     int chunks = n / p;
     double* x_i_data;
-    double* y_i_data;
+    double* y_i_send_data;
+    double* y_i_receive_data;
     int process_n;
     int process_m;
     int flag = -1;
@@ -227,106 +228,70 @@ void main() {
     // Every process has processed its own x_i_data, now they have to pass around data to each other in a ring mode
     // That has to repeat until each process has processed all possible data aka p times
 
-    // For the first iteration set the y_i_data array that will be passed
+    // For the first iteration set the y_i_send_data array that will be passed
     process_n = chunks;
-    y_i_data = malloc(process_n * d * sizeof(double));
-    y_i_data = x_i_data;
+    y_i_send_data = malloc(process_n * d * sizeof(double));
+    y_i_send_data = x_i_data;
+
+    // TODO: IMPLEMENT THE SEND AND RECEIVE AS DIFFERENT POINTERS TO DOUBLE ARRAYS
 
     for(int i = 0; i < p; i ++) {
         // Process zero sends first then waits for the last process
         if(world_rank != 0) {
             process_n = chunks;
-            y_i_data = malloc(process_n * d * sizeof(double));
-            MPI_Recv(y_i_data, process_n * d, MPI_DOUBLE, world_rank - 1, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-            knnresult temp_knnresult;
-            temp_knnresult.ndist = malloc(process_m * k * sizeof(double));
-            temp_knnresult.nidx = malloc(process_m * k * sizeof(int));
-
-            temp_knnresult = kNN(y_i_data, x_i_data, process_n, process_m, d, k, world_rank);
-
-            // We have the sub_knnresult from the previous comparisons and the temp_knnresult from the last knn operation
-            // We have to store in the sub_knnresult the smallest values from both
-            for(int i = 0; i < process_m; i++) {
-                double temp_dist[k];
-                int temp_index[k];            
-                for(int kappa = 0; kappa < k; kappa++) {
-                    int kappa_one = 0;
-                    int kappa_two = 0;
-                    if(temp_knnresult.ndist[kappa_one*process_m + i] < sub_knnresult.ndist[kappa_two*process_m + i]) {
-                        temp_dist[kappa] = temp_knnresult.ndist[kappa_one*process_m + i];
-                        temp_index[kappa] = temp_knnresult.nidx[kappa_one*process_m + i];
-                        kappa_one++;
-                    }
-                    else
-                    {
-                        temp_dist[kappa] =  sub_knnresult.ndist[kappa_two*process_m + i];
-                        temp_index[kappa] =  sub_knnresult.nidx[kappa_two*process_m + i];
-                        kappa_two++;
-                    }
-                }
-                for(int kappa = 0; kappa < k; kappa++) {
-                    sub_knnresult.ndist[kappa*process_m + i] = temp_dist[i];
-                    sub_knnresult.nidx[kappa*process_m + i] = temp_index[i];
-                }             
-            }
-
-
-            // Don't forget to clear temp_knnresult after this comment
-            free(temp_knnresult.ndist);
-            free(temp_knnresult.nidx);
+            y_i_receive_data = malloc(process_n * d * sizeof(double));
+            MPI_Recv(y_i_receive_data, process_n * d, MPI_DOUBLE, world_rank - 1, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
+
+
         
-        MPI_Send(y_i_data, process_n * d, MPI_DOUBLE, (world_rank + 1) % p, i, MPI_COMM_WORLD);
+        MPI_Send(x_i_data, process_n * d, MPI_DOUBLE, (world_rank + 1) % p, i, MPI_COMM_WORLD);
+        // After sending the previous y data that the process holds, in the next iteration we want to send the new data we just received
+        y_i_send_data = y_i_receive_data;
         if(world_rank == 0) {
             process_m = chunks;
-            y_i_data = malloc(process_n * d * sizeof(double));
-            MPI_Recv(y_i_data, process_n * d, MPI_DOUBLE, p - 1, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            
-            knnresult temp_knnresult;
-            temp_knnresult.ndist = malloc(process_m * k * sizeof(double));
-            temp_knnresult.nidx = malloc(process_m * k * sizeof(int));
-
-            temp_knnresult = kNN(y_i_data, x_i_data, process_n, process_m, d, k, world_rank);
-
-            // We have the sub_knnresult from the previous comparisons and the temp_knnresult from the last knn operation
-            // We have to store in the sub_knnresult the smallest values from both
-            for(int i = 0; i < process_m; i++) {
-                double temp_dist[k];
-                int temp_index[k];            
-                for(int kappa = 0; kappa < k; kappa++) {
-                    int kappa_one = 0;
-                    int kappa_two = 0;
-                    if(temp_knnresult.ndist[kappa_one*process_m + i] < sub_knnresult.ndist[kappa_two*process_m + i]) {
-                        temp_dist[kappa] = temp_knnresult.ndist[kappa_one*process_m + i];
-                        temp_index[kappa] = temp_knnresult.nidx[kappa_one*process_m + i];
-                        kappa_one++;
-                    }
-                    else
-                    {
-                        temp_dist[kappa] =  sub_knnresult.ndist[kappa_two*process_m + i];
-                        temp_index[kappa] =  sub_knnresult.nidx[kappa_two*process_m + i];
-                        kappa_two++;
-                    }
-                }
-                for(int kappa = 0; kappa < k; kappa++) {
-                    sub_knnresult.ndist[kappa*process_m + i] = temp_dist[i];
-                    sub_knnresult.nidx[kappa*process_m + i] = temp_index[i];
-                }             
-            }
-            
-            for(int i = 0; i < process_m * k; i++) {
-                if(i % process_m == 0) {
-                    printf("\n");
-                }
-                printf("%f \n", sub_knnresult.ndist[i]);
-            }
-            
-
-            // Don't forget to clear temp_knnresult after this comment
-            free(temp_knnresult.ndist);
-            free(temp_knnresult.nidx);
+            y_i_receive_data = malloc(process_n * d * sizeof(double));
+            MPI_Recv(y_i_receive_data, process_n * d, MPI_DOUBLE, p - 1, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
+
+        knnresult temp_knnresult;
+        temp_knnresult.ndist = malloc(process_m * k * sizeof(double));
+        temp_knnresult.nidx = malloc(process_m * k * sizeof(int));
+
+        temp_knnresult = kNN(y_i_receive_data, x_i_data, process_n, process_m, d, k, world_rank);
+
+        // THERE USED A BUG IN THE CODE BELOW
+        // I THINK I SOLVED IT THOUGH
+
+        // We have the sub_knnresult from the previous comparisons and the temp_knnresult from the last knn operation
+        // We have to store in the sub_knnresult the smallest values from both
+        for(int i = 0; i < process_m; i++) {
+            double temp_dist[k];
+            int temp_index[k];            
+            for(int kappa = 0; kappa < k; kappa++) {
+                int kappa_one = 0;
+                int kappa_two = 0;
+                if(temp_knnresult.ndist[kappa_one*process_m + i] < sub_knnresult.ndist[kappa_two*process_m + i]) {
+                    temp_dist[kappa] = temp_knnresult.ndist[kappa_one*process_m + i];
+                    temp_index[kappa] = temp_knnresult.nidx[kappa_one*process_m + i];
+                    kappa_one++;
+                }
+                else
+                {
+                    temp_dist[kappa] =  sub_knnresult.ndist[kappa_two*process_m + i];
+                    temp_index[kappa] =  sub_knnresult.nidx[kappa_two*process_m + i];
+                    kappa_two++;
+                }
+            }
+            for(int kappa = 0; kappa < k; kappa++) {
+                sub_knnresult.ndist[kappa*process_m + i] = temp_dist[kappa];
+                sub_knnresult.nidx[kappa*process_m + i] = temp_index[kappa];
+            }             
+        }
+
+        // Don't forget to clear temp_knnresult after this comment
+        free(temp_knnresult.ndist);
+        free(temp_knnresult.nidx);
     }
 
     // Send the sub_knn_results to zero process
@@ -350,26 +315,29 @@ void main() {
         for(int i = 1; i < p; i++) {
             double *temp = malloc(process_m * k * sizeof(double));
             MPI_Recv(temp, process_m * k, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            // for(int j = 0; j < process_m * k; j++) {
-            //     printf("%f\n", temp[j]);
-            // }
             
             // Position the elements in the appropriate position of the knnResult.ndist array
-            // for(int j = 0; j < process_m; j++) {
-            //     for(int kappa = 0; kappa < k; kappa++) {
-            //         knnResult.ndist[i * process_m + j + kappa*n] = temp[process_m * kappa + j];
-            //         printf("%d\n", i * process_m + j + kappa*n);
-            //         printf("%f\n", knnResult.ndist[i * process_m + j + kappa*n]);
-            //     }
-            // }
+            for(int j = 0; j < process_m; j++) {
+                for(int kappa = 0; kappa < k; kappa++) {
+                    knnResult.ndist[i * process_m + j + kappa*n] = temp[process_m * kappa + j];
+                    printf("%d\n", i * process_m + j + kappa*n);
+                    printf("%f\n", knnResult.ndist[i * process_m + j + kappa*n]);
+                }
+            }
+        }
+        // For i = 0
+        for(int j = 0; j < process_m; j++) {
+            for(int kappa = 0; kappa < k; kappa++) {
+                knnResult.ndist[j + kappa*n] = sub_knnresult.ndist[process_m * kappa + j];
+            }
         }
 
-        // for(int i = 0; i < n ; i++) {
-        //     if(i % n == 0) {
-        //         printf("\n");
-        //     }
-        //     printf("%f \n", knnResult.ndist[i]);
-        // }
+        for(int i = 0; i < n * k ; i++) {
+            if(i % n == 0) {
+                printf("\n");
+            }
+            printf("%d ", (int)knnResult.ndist[i]);
+        }
     }
 
 
