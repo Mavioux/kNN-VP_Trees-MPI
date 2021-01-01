8dee432ee67d8e6627f1b0ddaa86c134e2db21ad
mavioux <pakmarios@gmail.com>
2021-01-01 02:18:22 +0200

    first vpt_create implementation

--- src/v2.c
+++ src/v2.c
@@ -19,24 +19,126 @@ typedef struct knnresult{
 } knnresult;
 
 typedef struct node {
-    double data;
-    node* left;
-    node* right;
+    double* data;
+    double* up;
+    double mu;
+    struct node* left;
+    struct node* right;
 } node;
 
-struct node* newNode(doubla data) {
+// struct node* newNode(double up) {
 
-    // Allocate memory for new node
-    node* node = (node*)malloc(sizeof(node));
+//     // Allocate memory for new node
+//     node* node = malloc(sizeof(node));
 
-    // Assign data to this node
-    node->data = data;
+//     // Initialize left and right children as NULL
+//     node->left = NULL;
+//     node->right = NULL;
 
-    // Initialize left and right children as NULL
-    node->left = NULL;
-    node->right = NULL;
+//     return node;
+// }
 
-    return(node);
+void vpt_create(double* x_i_data, node* root, int m, int d) {
+    
+    if(m == 0) {
+        // root = malloc(sizeof(node));
+        // root->up = malloc(d * sizeof(double));
+        // for(int i = 0; i < d; i++) {
+        //     root->up[i] = x_i_data[i];
+        // }
+        return;
+    }
+
+    // Choosing the root node
+    root = malloc(sizeof(node));
+    root->up = malloc(d * sizeof(double));
+    for(int i = 0; i < d; i++) {
+        root->up[i] = x_i_data[i];
+    }
+
+    // Allocate space for left and right data
+    int left_size = 0;
+    int right_size = 0;
+    root->left = malloc(sizeof(node));
+    root->right = malloc(sizeof(node));
+
+    root->left->data = malloc(d * left_size * sizeof(double));
+    root->right->data = malloc(d * right_size * sizeof(double));
+
+    // Calculating the mu
+    double sum = 0;
+    for(int i = 1; i < m; i++) {
+        double temp_sum = 0;
+        for(int j = 0; j < d; j++){
+            temp_sum += x_i_data[j] * x_i_data[j] - x_i_data[i*d + j] * x_i_data[i*d + j];
+        }
+        if(temp_sum<0) {
+            temp_sum = -sqrt(abs(temp_sum));
+        }
+        else {
+            temp_sum = sqrt(temp_sum);
+        }
+        // printf("%f\n\n", temp_sum);
+        sum += temp_sum;
+    }
+    root->mu = sum / (m - 1);
+    // printf("%f\n", root->mu);
+
+    double value = 0;
+    for(int i = 1; i < m; i++) {
+        double temp_sum = 0;
+        for(int j = 0; j < d; j++){
+            temp_sum += x_i_data[j] * x_i_data[j] - x_i_data[i*d + j] * x_i_data[i*d + j];
+        }
+        if(temp_sum<0) {
+            temp_sum = -sqrt(abs(temp_sum));
+        }
+        else {
+            temp_sum = sqrt(temp_sum);
+        }
+        // printf("temp_sum: %f\tsum: %f \n\n", temp_sum, root->mu);
+        if(temp_sum < root->mu) {
+            left_size++;
+            root->left->data = realloc(root->left->data, d * left_size * sizeof(double));
+            for(int j = 0; j < d; j++) {
+                root->left->data[(left_size-1) * d + j] = x_i_data[i*d + j];
+            }  
+        }
+        else {
+            right_size++;
+            root->right->data = realloc(root->right->data, d * right_size * sizeof(double));
+            for(int j = 0; j < d; j++) {
+                root->right->data[(right_size-1) * d + j] = x_i_data[i*d + j];
+            }  
+        }
+    }
+
+    printf("\nRoot\n");
+    for(int j = 0; j < d; j++) {
+        printf("%f\n", root->up[j]);
+    }
+    printf("\nLeft Child\n");
+    for(int i = 0; i < left_size; i++) {
+        for(int j = 0; j < d; j++) {
+            printf("%f\n", root->left->data[i*d+j]);
+        }
+        printf("\n");
+    }
+    printf("\nRight Child\n");
+    for(int i = 0; i < right_size; i++) {
+        for(int j = 0; j < d; j++) {
+            printf("%f\n", root->right->data[i*d+j]);
+        }
+        printf("\n");
+    }
+
+    
+
+
+
+    vpt_create(root->left->data, root->left, left_size, d);
+    vpt_create(root->right->data, root->right, right_size, d);
+
 }
 
 double randomReal(double low, double high) {
@@ -59,7 +161,7 @@ void main() {
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    int n = 1000;
+    int n = 20;
     int d = 2;
     int k = 3;
 
@@ -86,7 +188,7 @@ void main() {
         // Create an X array n x d
         for(int i = 0; i < n * d; i++) {
             // x_data[i] = randomReal(0, 10);
-            x_data[i] = 2*i;
+            x_data[i] = i;
         }
 
         if(p > 1) {
@@ -109,6 +211,10 @@ void main() {
         sub_knnresult.n = process_m;
         sub_knnresult.ndist = malloc(process_m * k * sizeof(double));
         sub_knnresult.nidx = malloc(process_m * k * sizeof(int));
+
+        // We can now create the vpt tree of the elements of process zero
+        node* root;
+        vpt_create(x_i_data, root, process_m, d);
     }
     else if (world_rank == p - 1)
     {
@@ -147,122 +253,114 @@ void main() {
     // Now they have to pass around data to each other in a ring mode
     // That has to repeat until each process has processed all possible data aka p times
 
-    // For the first iteration set the y_i_send_data array that will be passed
-    process_n = chunks;
-    y_i_send_data = malloc(process_n * d * sizeof(double));
-    //REALLY IMPORTANT NOT TO SET THE POINTERS EQUAL HERE
-    memcpy(y_i_send_data, x_i_data, process_m * d * sizeof(double));
+    // // For the first iteration set the y_i_send_data array that will be passed
+    // process_n = chunks;
+    // y_i_send_data = malloc(process_n * d * sizeof(double));
+    // //REALLY IMPORTANT NOT TO SET THE POINTERS EQUAL HERE
+    // memcpy(y_i_send_data, x_i_data, process_m * d * sizeof(double));
 
-    for(int i = 0; i < p; i ++) {
-        // Process zero sends first then waits for the last process
-        if(world_rank != 0) {
-            process_n = chunks;
-            y_i_receive_data = malloc(process_n * d * sizeof(double));
-            MPI_Recv(y_i_receive_data, process_n * d, MPI_DOUBLE, world_rank - 1, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
+    // for(int i = 0; i < p; i ++) {
+    //     // Process zero sends first then waits for the last process
+    //     if(world_rank != 0) {
+    //         process_n = chunks;
+    //         y_i_receive_data = malloc(process_n * d * sizeof(double));
+    //         MPI_Recv(y_i_receive_data, process_n * d, MPI_DOUBLE, world_rank - 1, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    //     }
 
 
         
-        MPI_Send(y_i_send_data, process_n * d, MPI_DOUBLE, (world_rank + 1) % p, i, MPI_COMM_WORLD);
-        if(world_rank != 0) {
-            // After sending the previous y data that the process holds, in the next iteration, we want to send the new data we just received
-            memcpy(y_i_send_data, y_i_receive_data, process_m * d * sizeof(double));
-        }
-        if(world_rank == 0) {
-            process_m = chunks;
-            y_i_receive_data = malloc(process_n * d * sizeof(double));
-            MPI_Recv(y_i_receive_data, process_n * d, MPI_DOUBLE, p - 1, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            // After sending the previous y data that the process holds, in the next iteration, we want to send the new data we just received
-            memcpy(y_i_send_data, y_i_receive_data, process_m * d * sizeof(double));
-        }
+    //     MPI_Send(y_i_send_data, process_n * d, MPI_DOUBLE, (world_rank + 1) % p, i, MPI_COMM_WORLD);
+    //     if(world_rank != 0) {
+    //         // After sending the previous y data that the process holds, in the next iteration, we want to send the new data we just received
+    //         memcpy(y_i_send_data, y_i_receive_data, process_m * d * sizeof(double));
+    //     }
+    //     if(world_rank == 0) {
+    //         process_m = chunks;
+    //         y_i_receive_data = malloc(process_n * d * sizeof(double));
+    //         MPI_Recv(y_i_receive_data, process_n * d, MPI_DOUBLE, p - 1, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    //         // After sending the previous y data that the process holds, in the next iteration, we want to send the new data we just received
+    //         memcpy(y_i_send_data, y_i_receive_data, process_m * d * sizeof(double));
+    //     }
 
-        knnresult temp_knnresult;
-        temp_knnresult.ndist = malloc(process_m * k * sizeof(double));
-        temp_knnresult.nidx = malloc(process_m * k * sizeof(int));
+    //     knnresult temp_knnresult;
+    //     temp_knnresult.ndist = malloc(process_m * k * sizeof(double));
+    //     temp_knnresult.nidx = malloc(process_m * k * sizeof(int));
         
-        if(!knn_counter) {
-            // First call of knn so we store the result in the permanent sub_knnresult
-            sub_knnresult = kNN(y_i_receive_data, x_i_data, process_n, process_m, d, k, world_rank);
-            knn_counter++;
-        }
-        else {
-            temp_knnresult = kNN(y_i_receive_data, x_i_data, process_n, process_m, d, k, world_rank);
-            // We have the sub_knnresult from the previous comparisons and the temp_knnresult from the last knn operation
-            // We have to store in the sub_knnresult the smallest values from both
-            for(int i = 0; i < process_m; i++) {
-                double temp_dist[k];
-                int temp_index[k];    
-                int kappa_one = 0;
-                int kappa_two = 0;        
-                for(int kappa = 0; kappa < k; kappa++) {
-                    if(temp_knnresult.ndist[kappa_one*process_m + i] < sub_knnresult.ndist[kappa_two*process_m + i]) {
-                        temp_dist[kappa] = temp_knnresult.ndist[kappa_one*process_m + i];
-                        temp_index[kappa] = temp_knnresult.nidx[kappa_one*process_m + i];
-                        kappa_one++;
-                    }
-                    else
-                    {
-                        temp_dist[kappa] =  sub_knnresult.ndist[kappa_two*process_m + i];
-                        temp_index[kappa] =  sub_knnresult.nidx[kappa_two*process_m + i];
-                        kappa_two++;
-                    }
-                }
-                for(int kappa = 0; kappa < k; kappa++) {
-                    sub_knnresult.ndist[kappa*process_m + i] = temp_dist[kappa];
-                    sub_knnresult.nidx[kappa*process_m + i] = temp_index[kappa];
-                }             
-            }
+    //     if(!knn_counter) {
+    //         // First call of knn so we store the result in the permanent sub_knnresult
+    //         sub_knnresult = kNN(y_i_receive_data, x_i_data, process_n, process_m, d, k, world_rank);
+    //         knn_counter++;
+    //     }
+    //     else {
+    //         temp_knnresult = kNN(y_i_receive_data, x_i_data, process_n, process_m, d, k, world_rank);
+    //         // We have the sub_knnresult from the previous comparisons and the temp_knnresult from the last knn operation
+    //         // We have to store in the sub_knnresult the smallest values from both
+    //         for(int i = 0; i < process_m; i++) {
+    //             double temp_dist[k];
+    //             int temp_index[k];    
+    //             int kappa_one = 0;
+    //             int kappa_two = 0;        
+    //             for(int kappa = 0; kappa < k; kappa++) {
+    //                 if(temp_knnresult.ndist[kappa_one*process_m + i] < sub_knnresult.ndist[kappa_two*process_m + i]) {
+    //                     temp_dist[kappa] = temp_knnresult.ndist[kappa_one*process_m + i];
+    //                     temp_index[kappa] = temp_knnresult.nidx[kappa_one*process_m + i];
+    //                     kappa_one++;
+    //                 }
+    //                 else
+    //                 {
+    //                     temp_dist[kappa] =  sub_knnresult.ndist[kappa_two*process_m + i];
+    //                     temp_index[kappa] =  sub_knnresult.nidx[kappa_two*process_m + i];
+    //                     kappa_two++;
+    //                 }
+    //             }
+    //             for(int kappa = 0; kappa < k; kappa++) {
+    //                 sub_knnresult.ndist[kappa*process_m + i] = temp_dist[kappa];
+    //                 sub_knnresult.nidx[kappa*process_m + i] = temp_index[kappa];
+    //             }             
+    //         }
 
-            // Don't forget to clear temp_knnresult after this comment
-            free(temp_knnresult.ndist);
-            free(temp_knnresult.nidx);
-        }                
-    }
+    //         // Don't forget to clear temp_knnresult after this comment
+    //         free(temp_knnresult.ndist);
+    //         free(temp_knnresult.nidx);
+    //     }                
+    // }
 
-    // Send the sub_knn_results to zero process
-    if(world_rank != 0) {
-        MPI_Send(sub_knnresult.ndist, process_m * k, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-    }
-    else {
-        knnresult knnResult;
-        knnResult.nidx = malloc(n * k * sizeof(int));
-        if(knnResult.nidx == NULL) {
-            printf("error allocating memory\n");
-            exit(0);
-        }
-        knnResult.ndist = malloc(n * k * sizeof(double));
-        if(knnResult.ndist == NULL) {
-            printf("error allocating memory\n");
-            exit(0);
-        }
-        knnResult.n = n;
-        knnResult.k = k;
-        for(int i = 1; i < p; i++) {
-            double *temp = malloc(process_m * k * sizeof(double));
-            MPI_Recv(temp, process_m * k, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    // // Send the sub_knn_results to zero process
+    // if(world_rank != 0) {
+    //     MPI_Send(sub_knnresult.ndist, process_m * k, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    // }
+    // else {
+    //     knnresult knnResult;
+    //     knnResult.nidx = malloc(n * k * sizeof(int));
+    //     if(knnResult.nidx == NULL) {
+    //         printf("error allocating memory\n");
+    //         exit(0);
+    //     }
+    //     knnResult.ndist = malloc(n * k * sizeof(double));
+    //     if(knnResult.ndist == NULL) {
+    //         printf("error allocating memory\n");
+    //         exit(0);
+    //     }
+    //     knnResult.n = n;
+    //     knnResult.k = k;
+    //     for(int i = 1; i < p; i++) {
+    //         double *temp = malloc(process_m * k * sizeof(double));
+    //         MPI_Recv(temp, process_m * k, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             
-            // Position the elements in the appropriate position of the knnResult.ndist array
-            printf("\n");
-            for(int j = 0; j < process_m; j++) {
-                for(int kappa = 0; kappa < k; kappa++) {
-                    printf("%f\n", temp[process_m * kappa + j]);
-                    knnResult.ndist[i * process_m + j + kappa*n] = temp[process_m * kappa + j];
-                    printf("%d\n", i * process_m + j + kappa*n);
-                }
-            }
-        }
-        // For i = 0
-        for(int j = 0; j < process_m; j++) {
-            for(int kappa = 0; kappa < k; kappa++) {
-                knnResult.ndist[j + kappa*n] = sub_knnresult.ndist[process_m * kappa + j];
-            }
-        }
-
-        // printf("FINAL KNN");
-        // for(int i = 0; i < n * k ; i++) {
-        //     printf("%d %f \n",i, knnResult.ndist[i]);
-        // }
-    }
+    //         // Position the elements in the appropriate position of the knnResult.ndist array
+    //         for(int j = 0; j < process_m; j++) {
+    //             for(int kappa = 0; kappa < k; kappa++) {
+    //                 knnResult.ndist[i * process_m + j + kappa*n] = temp[process_m * kappa + j];
+    //             }
+    //         }
+    //     }
+    //     // For i = 0
+    //     for(int j = 0; j < process_m; j++) {
+    //         for(int kappa = 0; kappa < k; kappa++) {
+    //             knnResult.ndist[j + kappa*n] = sub_knnresult.ndist[process_m * kappa + j];
+    //         }
+    //     }
+    // }
 
     // Finalize the MPI environment.
     MPI_Finalize();
